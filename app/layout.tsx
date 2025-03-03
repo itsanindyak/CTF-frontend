a41a7756876b5e45089d863b07be388b688d36e4
itsanindyak <iankoley04@gmail.com>
2025-03-03 17:46:12 +0530

    Remove unused font imports and add Toaster component for notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Toaster } from "sonner";
+import { usePathname } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,13 +14,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
+      <body className={`antialiased`}>
+        <Navbar/>
         {children}
+        <Toaster richColors />
       </body>
     </html>
   );
