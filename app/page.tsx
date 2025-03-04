a1d04eee421f6ddd163b3df21bcec3ff6058b07d
itsanindyak <iankoley04@gmail.com>
2025-03-04 20:37:54 +0530

    Add background gradient and restructure home page layout for improved aesthetics

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,62 @@
+import React from "react";
+import "./index.css";
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/Button";
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className=" pb-7 relative min-h-screen  flex flex-row overflow-hidden bg font-[Unlock]">
+      <div className=" pt-4 w-11/12 lg:w-9/12 flex flex-col items-center gap-y-2 bg-[#F5F0E8] rounded-br-4xl">
+        <div className="my-8 flex flex-col md:flex-row w-8/12 md:w-full justify-center items-center gap-x-5 gap-y-2.5">
+          <Image src="/Xpedition.png" width={200} height={200} alt="logo" />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <div className="w-full md:w-fit flex justify-end gap-x-2 md:gap-x-5">
+            <p>By</p>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/sccse_logo.png"
+              width={120}
+              height={120}
+              alt="logo"
+              className=""
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <div className="flex flex-col w-full h-full justify-center items-center gap-y-4">
+          <Button
+            size="lg"
+            className="bg-[#f5f5dc] text-2xl sm:text-4xl w-6/12 sm:w-7/12 text-amber-900 border border-amber-800 hover:bg-amber-100 px-8 py-2 sm:py-8 rounded-md font-medium transition-all"
+          >
+            Day 1
+          </Button>
+
+          <Button className="bg-[#f5f5dc] text-2xl sm:text-4xl w-6/12 sm:w-7/12 text-amber-900 border border-amber-800 hover:bg-amber-100 px-8 py-2 sm:py-8 rounded-md font-medium transition-all">
+            Day 2
+          </Button>
+        </div>
+      </div>
+
+      <div className="invisible lg:visible absolute top-[40%] left-[65%] md:right-[20%] w-[150px] md:w-[250px] ">
+        <Image
+          src="/Group 17.png"
+          width={100}
+          height={100}
+          alt="map"
+          className="h-auto w-full"
+        />
+        <div className="absolute -top-[45%] -right-[45%] w-[120px] md:w-[160px] ">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/treasure_box .png"
+            width={100}
+            height={100}
+            alt="treasure_box"
+            className="h-auto w-full"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default page;
