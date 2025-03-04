d83cdffc95da53ab7b51227751f21adb4f4b2ece
itsanindyak <iankoley04@gmail.com>
2025-03-04 20:38:00 +0530

    Hide navbar on home page in addition to login and registration routes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     { name: "Leaderboard", path: "/leaderboard" },
   ];
 
-  const hideNavbarRoutes = ["/login","/register"];
+  const hideNavbarRoutes = ["/login","/register","/"];
 
   if (hideNavbarRoutes.includes(pathname)) return null;
 
