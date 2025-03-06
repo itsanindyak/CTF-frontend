d5ebc033e77a59997e86592f9b508fc3ce477aef
itsanindyak <iankoley04@gmail.com>
2025-03-06 21:55:43 +0530

    Add UserContext for managing team name state across components

---
+++ context/UserContext.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { error } from "console";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type UserContextType = {
+  teamName: string;
+  setTeamName: (teamName: string) => void;
+};
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [teamName, setTeamName] = useState("");
+
+  return (
+    <UserContext.Provider value={{ teamName, setTeamName }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser miust be within Userprovider.");
+  }
+  return context;
+};
+
+export { UserContext, UserProvider, useUser };
